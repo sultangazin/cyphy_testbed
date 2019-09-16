addea6aef4b742022e1afdd6573218d57ed862be
Luigi Pannocchi <l.pannocchi@gmail.com>
2019-09-16 00:19:43 -0700

    Starting reformat the python class with inheritance

--- ws/src/rarena/rarena.py
+++ ws/src/rarena/rarena.py
@@ -14,6 +14,8 @@ from geometry_msgs.msg import PoseStamped
 import numpy as np
 
 
+activeDrone = None
+
 ###### HELPERS #####
 def posFromPoseMsg(pose_msg):
     pos = np.array([pose_msg.pose.position.x, 
@@ -29,26 +31,38 @@ def quatFromPoseMsg(pose_msg):
     return quat 
 
 
+def toggle_active(name):
+    if (activeDrone == name):
+        activeDrone = None
+    else:
+        activeDrone = name
+
+
+def issue_command(tg_p):
+    if (activeDrone == None):
+        print("No drone selected!")
+    else:
+        try:
+            resp1 = drones[active].goTo([tg_p[0], tg_p[1], tg_p[0]], 2.0)
+        except rospy.ServiceException as exc:
+            print("Service did not process request: " + str(exc))
+
 
 
 ################# ROS ARENA CLASS #####################
 class RArenaClass:
-
+    """
+    Bridge class between Ros and Arena.
+    """
     def __init__(self, mqtt_client, scene, obj_name, obj_type, position=[0,0,0], scale=[1,1,1]):
 
         self.client = mqtt_client
 
         self.initData(scene, obj_name, obj_type, position, scale)
 
-        self.loadParameters()
+        self.initArenaObject()
 
-        self.registerCallbacks()
-
-        self.registerServices()
-
-        self.initArena()
-
-        rospy.loginfo("\n [%s] RArena Initialized!"%rospy.get_name()) 
+        rospy.loginfo("\n [%s] RArena Object Initialized!"%rospy.get_name()) 
 
     def __del__(self):
         self.client.publish(self.scene + self.object_name, "", retain=True)  
@@ -60,15 +74,8 @@ class RArenaClass:
         self.scene_path = scene
 
         self.object_name = obj_name
-        self.object_type = obj_type
+        self.object_type = obj_type 
         
-        if (self.object_type == "drone"):
-            print("Creating Arena Drone Object")
-            self.color = "#0000FF"
-        else:
-            print("Creating Arena Target Object")
-            self.color = "#FFFFFF"
-
         self.base_str = ",{},{},{},{},{},{},{},{},{},{},{},on"
 
         self.obj_str = self.object_name + self.base_str 
@@ -79,15 +86,8 @@ class RArenaClass:
         self.has_datasource = False
  
         
-    def loadParameters(self):
-        # Check whether the param specifying the pose topic is defined
-        #if rospy.has_param('/' + self.object_name + '/aggregator/topics/out_ext_pose_topic'):
-        if (self.object_type == "drone"):
-            self.has_datasource = True
-            self.obj_pose_topic_ =  "/cf1/external_pose"
 
-
-    def initArena(self):
+    def initArenaObject(self):
         
         ############
         # Setup box
@@ -113,37 +113,23 @@ class RArenaClass:
         self.client.message_callback_add(self.scene_path + self.object_name + "/mousedown", self.on_click_input)
 
 
-    def registerCallbacks(self):
-        if (self.has_datasource):
-            # Subscribe to vehicle state update
-            print("Subscribing to ", self.obj_pose_topic_)
-            rospy.Subscriber(self.obj_pose_topic_, 
-                    PoseStamped, self.pose_callback)
-
-    def registerServices(self):
-        rospy.wait_for_service('/cf1/Commander_Node/goTo_srv')
-        self.goTo = rospy.ServiceProxy('/cf1/Commander_Node/goTo_srv', GoTo)
-        #self.service_callback =  srv_call
-        pass
-        
-
-    ###### CALLBACKS
-    # On new target information
-    def pose_callback(self, pose_msg):
-        pos = posFromPoseMsg(pose_msg)
-        quat = quatFromPoseMsg(pose_msg)
-
-        # Update the position of the object
+               
+    def plot_arenaObj(pos, quaternion):
+        """
+        Update the coordinates of an object in arena and
+        plot it in the synthetic environment
+        """
+        # Update the pos of the object
         self.p[0] = float(pos[0])
         self.p[1] = float(pos[1])
         self.p[2] = float(pos[2])
-        
+
         tg_scene_string = self.scene_path + self.object_name
         cmd_string = self.obj_str.format(
                     str(pos[0]), 
                     str(pos[2]), 
                     str(-pos[1]), 
-                    str(quat[1]), str(-quat[3]), str(quat[2]), str(quat[0]), 
+                    str(quaterion[1]), str(-quaternion[3]), str(quaternion[2]), str(quaternion[0]), 
                     self.scale[0], self.scale[1], self.scale[2],
                     str(self.color))
 
@@ -157,29 +143,97 @@ class RArenaClass:
 
         return
 
-
     def on_click_input(self, client, userdata, msg):
 
         print("Got click: %s \"%s\"" % (msg.topic, msg.payload))
         click_x, click_y, click_z, user = msg.payload.split(',')
         print("Clicked by: " + user)    
 
-        if (self.object_type == "target"):
-            # Create array with the target position
-            tg_p = np.array([float(click_x), float(click_y), float(click_z)])
+        
 
-            if str(msg.topic).find("mousedown") != -1:
-                print( "Target Position: " + str(tg_p[0]) + "," + str(tg_p[1]) + "," + str(tg_p[2]) )
-                
-                try:
-                    resp1 = self.goTo([tg_p[0], -tg_p[2], tg_p[1] + 1.0], 2.0)
-                except rospy.ServiceException as exc:
-                    print("Service did not process request: " + str(exc))
+###### DRONE AREAN CLASS ########
+def DroneArenaClass(RArenaClass):
+    def __init__(self, mqtt_client, scene, obj_name):
+        print("Creating Arena Drone Object")
 
-                tg_string = "TargetGoto" + self.base_str;
-                self.client.publish(self.scene_path + "TargetGoto", tg_string.format(str(tg_p[0]), str(tg_p[1] + 1.0), str(tg_p[2]), 0.0, 0.0, 0.0, 0.0,
-                    0.1, 0.1, 0.1, "#FF0000"), retain=True) 
+        self.name = name
+        self.isActive = False
+ 
+        # Name of the topic containing the pose of the object       
+        self.obj_pose_topic_ =  "/" + name + "/external_pose"
 
-        if (self.object_type == "drone"):
-            print("Drone Selected\n")
+        super().__init__(self, mqtt_client, scene, obj_name = self.name, obj_type = "drone", position=[0,0,0], scale=[1,1,1]):  
+        super().color = "#0000FF"
 
+        self.registerCallbacks()
+
+        self.registerServices()
+
+
+    def registerCallbacks(self):
+        # Subscribe to vehicle state update
+        print("Subscribing to ", self.obj_pose_topic_)
+        rospy.Subscriber(self.obj_pose_topic_, 
+                PoseStamped, self.pose_callback)
+       
+
+    def pose_callback(self, pose_msg):
+        position = posFromPoseMsg(pose_msg)
+        quaternion = quatFromPoseMsg(pose_msg)
+        
+        # Plot the object in Arena
+        super().plot_arenaObj(position, quaternion)
+
+        
+    def registerServices(self):
+        rospy.wait_for_service("/" + self.name + "/Commander_Node/goTo_srv")
+        self.goTo = rospy.ServiceProxy("/" + self.name + "/Commander_Node/goTo_srv", GoTo)
+        pass
+
+
+    def on_click_input(self, client, userdata, msg):
+        click_x, click_y, click_z, user = msg.payload.split(',')
+    
+        if (self.isActive):
+            self.isActive = False
+            super().color("#000022")
+            print("Drone {} Deselected\n".format(self.name))
+        else:
+            self.isActive = True
+            super().color("#0000FF")
+            print("Drone {} Selected\n".format(self.name))
+
+        toggle_active(self.name) 
+ 
+
+            
+###### TARGET ARENA CLASS #######
+def TargetArenaClass(RArenaClass):
+    def __init__(self, name):
+        super().color = "#FFFFFF"
+        print("Creating Arena Target Object")
+
+        self.name = name
+
+        super().__init__(self, mqtt_client, scene, obj_name = self.name, obj_type = "target", position=[0,0,0], scale=[1,1,1]):  
+        super().color = "#0000FF"
+
+
+    def on_click_input(self, client, userdata, msg):
+        click_x, click_y, click_z, user = msg.payload.split(',')
+
+        # Create array with the target position
+
+        if str(msg.topic).find("mousedown") != -1:
+            print( "Target Position: " + str(click_x) + "," + str(click_y) + "," + str(click_z) )
+            
+            tg_string = "TargetGoto" + self.base_str;
+            super().client.publish(self.scene_path + "TargetGoto", tg_string.format(click_x, click_y, click_z), 0.0, 0.0, 0.0, 0.0,
+                0.1, 0.1, 0.1, "#FF0000"), retain=True) 
+
+            tg_p = np.array([float(click_x), -float(click_z), float(click_y)])
+            issue_command(tg_p)
+                        
+
+
+drones = dict('cf1', DroneArenaClass(mqtt_client, "topic/luigi", "cf1"))
