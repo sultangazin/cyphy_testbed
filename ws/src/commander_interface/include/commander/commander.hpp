a57d171c068505a650d49d1d79f8ba73aec117f2
Luigi Pannocchi <l.pannocchi@gmail.com>
2019-09-29 09:16:26 -0700

    Added flip command; Fixed indentation

--- ws/src/commander_interface/include/commander/commander.hpp
+++ ws/src/commander_interface/include/commander/commander.hpp
@@ -8,55 +8,61 @@
 #include "commander_interface/Track.h"
 #include "commander_interface/GoTo.h"
 #include "commander_interface/Impact.h"
+#include "commander_interface/Flip.h"
 
 // =================================================================
 // CLASS
 //
 
 class CommanderInterface {
-        public:
-                CommanderInterface();
-                ~CommanderInterface();
+    public:
+        CommanderInterface();
+        ~CommanderInterface();
 
-                bool Initialize(const ros::NodeHandle& n);
+        bool Initialize(const ros::NodeHandle& n);
 
-                bool takeoff_callback( 
-                                commander_interface::TakeOff::Request  &req,
-                                commander_interface::TakeOff::Response &res);
-                bool land_callback(
-                                commander_interface::Land::Request  &req,
-                                commander_interface::Land::Response &res);
+        bool takeoff_callback( 
+                commander_interface::TakeOff::Request  &req,
+                commander_interface::TakeOff::Response &res);
+        bool land_callback(
+                commander_interface::Land::Request  &req,
+                commander_interface::Land::Response &res);
 
-                bool goto_callback(
-                                commander_interface::GoTo::Request  &req,
-                                commander_interface::GoTo::Response &res);
+        bool goto_callback(
+                commander_interface::GoTo::Request  &req,
+                commander_interface::GoTo::Response &res);
 
-                bool track_callback(
-                                commander_interface::Track::Request  &req,
-                                commander_interface::Track::Response &res);
+        bool track_callback(
+                commander_interface::Track::Request  &req,
+                commander_interface::Track::Response &res);
 
-                bool impact_callback(
-                                commander_interface::Impact::Request  &req,
-                                commander_interface::Impact::Response &res);
+        bool impact_callback(
+                commander_interface::Impact::Request  &req,
+                commander_interface::Impact::Response &res);
 
-        private:
+        bool flip_callback(
+                commander_interface::Flip::Request &req,
+                commander_interface::Flip::Response &res);
+
+
+                private:
 
                 // Load Parameters
                 bool LoadParameters(const ros::NodeHandle& n);
-//                bool RegisterCallbacks(const ros::NodeHandle& n);
+                //                bool RegisterCallbacks(const ros::NodeHandle& n);
 
                 // ROS variables
                 //
                 // Service Server
-                
                 ros::ServiceServer takeoff_srv_;
                 ros::ServiceServer land_srv_;
                 ros::ServiceServer goTo_srv_;
                 ros::ServiceServer track_srv_;
                 ros::ServiceServer impact_srv_;
+                ros::ServiceServer flip_srv_;
+
                 // Service Client
                 ros::ServiceClient guidance_clnt_;
-
 
 
                 // Callbacks
