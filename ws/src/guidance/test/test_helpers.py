ec12b608258cefc5ffef0312621c82031b1e03ea
Luigi Pannocchi <l.pannocchi@gmail.com>
2019-10-09 21:27:51 -0700

    Added test for the obstacle avoidance

--- ws/src/guidance/test/test_helpers.py
+++ ws/src/guidance/test/test_helpers.py
@@ -6,6 +6,8 @@ import numpy as np
 
 from guidance_helper import *
 
+print("\n\n================ ")
+print("Test Integration ")
 DT = 1.0
 
 Vi = np.array([1.0, 0.0, 0.0])
@@ -16,11 +18,28 @@ Vf = np.array([1.0, 0.0, Vi[2] + DT * Af[2]])
 
 angle = -math.pi/2
 
-qt = np.array([math.cos(angle), 0, math.sin(angle), 0]
+qt = np.array([math.cos(angle), 0, math.sin(angle), 0])
 
-Y = Integration(Xf, Vf, Af, DT, 0.001, -1, qt)  
+Y = Integration(Xf, Vf, Af, DT, 0.001, -1) 
 
 print("X0 = ", Y[0:3])
 print("V0 = ", Y[3:6])
+
+print("\n\n================ ")
+print("Test Obstacle Avoidance ")
+
+
+p0 = np.array([0.1, 0.1, 0.1])
+pf = np.array([1, 0.1, 0])
+po = np.array([0.5, 0.0, 0.0])
+md = 0.3
+(isInt, e) = evalObstacleInt(p0, pf, po, md)
+
+if (isInt):
+    print("Intersecting")
+    genAvoidWaypoints(p0, pf, po, md)
+else:
+    print("Not intersecting")
+
 
 
