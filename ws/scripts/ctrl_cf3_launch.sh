2a1cb7b764c0b6fabd0936be7086eedd6abda4d0
Luigi Pannocchi <l.pannocchi@gmail.com>
2020-02-23 13:32:42 -0800

    Added scripts to simplify the launching of different components

---
+++ ws/scripts/ctrl_cf3_launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+roslaunch demo_launchers controller_launch.launch vehicle_frame:=cf3 uri:=radio://0/80/2M/E7E7E7E703 server:=RS2
---
+++ ws/scripts/radio_launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+roslaunch demo_launchers radio_server.launch rs_name:=RS2
