9334030707d0822b685d19441fbeea94097ae17e
Luigi Pannocchi <l.pannocchi@gmail.com>
2020-07-27 12:21:36 -0700

    Updated script

--- ws/scripts/gotos.sh
+++ ws/scripts/gotos.sh
@@ -10,9 +10,9 @@ rosservice call /cf3/Commander_Node/goTo_srv "[0.8, 0.2, 0.7]" "4.0"
 sleep 4
 rosservice call /cf3/Commander_Node/goTo_srv "[0.0, 0.0, 0.7]" "4.0"
 sleep 4
-rosservice call /cf3/Commander_Node/goTo_srv "[-0.5, 0.0, 0.7]" "3.0"
+rosservice call /cf3/Commander_Node/goTo_srv "[-0.8, 0.0, 0.7]" "3.0"
 sleep 3
 rosservice call /cf3/Commander_Node/goTo_srv "[0.1, -0.5, 0.7]" "3.0"
 sleep 3
-rosservice call /landAll "{}"
+rosservice call /cf3/Commander_Node/land_srv "3.0" "[0.0, -0.5, 0.0]"
 sleep 3
