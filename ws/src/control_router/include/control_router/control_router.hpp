69bf98e5f7b74b4768a2b84a2b826d05ace7a9fb
rt-2pm2 <l.pannocchi@gmail.com>
2021-01-28 04:42:10 -0800

    Made the control router more dynamics

--- ws/src/control_router/include/control_router/control_router.hpp
+++ ws/src/control_router/include/control_router/control_router.hpp
@@ -2,6 +2,10 @@
 #define COMMANDER_INTERFACE_H
 
 #include <mutex>
+#include <thread>
+#include <string>
+#include <unordered_map>
+
 #include "ros/ros.h"
 
 #include "control_router/SelectController.h"
@@ -9,78 +13,79 @@
 #include <testbed_msgs/ControlStamped.h>
 #include <crazyflie_driver/PWM.h>
 #include "control_router/NetworkStatusMsg.h"
-#include <mutex>
+
+#include "utilities/network_parser/network_parser.hpp"
 
 #define DEFAULT_NUM_ELEMENTS (50)
 
 struct AveragingFilter {
-    std::mutex mx;
-    std::vector<double> meas;
-    int BufferSize;
-    unsigned int head;
-    unsigned int tail;
-    int NumOfElements;
+	std::mutex mx;
+	std::vector<double> meas;
+	int BufferSize;
+	unsigned int head;
+	unsigned int tail;
+	int NumOfElements;
 
-    AveragingFilter() : meas(std::vector<double>(DEFAULT_NUM_ELEMENTS, 0)) {
-        BufferSize = DEFAULT_NUM_ELEMENTS;
-        head = 0;
-        tail = 0;
-        NumOfElements = 0;
-    }
+	AveragingFilter() : meas(std::vector<double>(DEFAULT_NUM_ELEMENTS, 0)) {
+		BufferSize = DEFAULT_NUM_ELEMENTS;
+		head = 0;
+		tail = 0;
+		NumOfElements = 0;
+	}
 
-    AveragingFilter(int n) : meas(std::vector<double>(n, 0)) {
-        BufferSize = n;
-        head = 0;
-        tail = 0;
-        NumOfElements = 0;
-    }
+	AveragingFilter(int n) : meas(std::vector<double>(n, 0)) {
+		BufferSize = n;
+		head = 0;
+		tail = 0;
+		NumOfElements = 0;
+	}
 
-    AveragingFilter(AveragingFilter&& af) : meas(std::vector<double>(af.BufferSize, 0)) {
-        BufferSize = af.BufferSize;
-        head = 0;
-        tail = 0;
-        NumOfElements = 0;
-    }
+	AveragingFilter(AveragingFilter&& af) : meas(std::vector<double>(af.BufferSize, 0)) {
+		BufferSize = af.BufferSize;
+		head = 0;
+		tail = 0;
+		NumOfElements = 0;
+	}
 
-    double getAvg() {
-        double output = 0;
-        mx.lock();
-        if (NumOfElements < 2) {
-            mx.unlock();
-            return 0;
-        }
-        output =(meas[head] - meas[tail]) / (NumOfElements - 1); 
-        mx.unlock();
-        return output;
-    }
+	double getAvg() {
+		double output = 0;
+		mx.lock();
+		if (NumOfElements < 2) {
+			mx.unlock();
+			return 0;
+		}
+		output =(meas[head] - meas[tail]) / (NumOfElements - 1); 
+		mx.unlock();
+		return output;
+	}
 
-    double getAvgFreq() {
-        double output = 0;
-        mx.lock();
-        if (NumOfElements < 2) {
-            mx.unlock();
-            return 0;
-        }
-        output = (NumOfElements - 1)/(meas[head] - meas[tail]);
-        mx.unlock();
-        return output;
-    }
+	double getAvgFreq() {
+		double output = 0;
+		mx.lock();
+		if (NumOfElements < 2) {
+			mx.unlock();
+			return 0;
+		}
+		output = (NumOfElements - 1)/(meas[head] - meas[tail]);
+		mx.unlock();
+		return output;
+	}
 
-    void addMeas(double m) {
-        mx.lock();
-        if (NumOfElements == 0) {
-            NumOfElements++;
-            meas.at(head) = m;
-        } else {
-            head = (head  + 1) % BufferSize;
-            meas.at(head) = m;
-            NumOfElements = (NumOfElements < BufferSize) ? NumOfElements + 1 : BufferSize;
-            if (head == tail) {
-                tail = (tail + 1) % BufferSize;
-            }
-        }
-        mx.unlock();
-    }
+	void addMeas(double m) {
+		mx.lock();
+		if (NumOfElements == 0) {
+			NumOfElements++;
+			meas.at(head) = m;
+		} else {
+			head = (head  + 1) % BufferSize;
+			meas.at(head) = m;
+			NumOfElements = (NumOfElements < BufferSize) ? NumOfElements + 1 : BufferSize;
+			if (head == tail) {
+				tail = (tail + 1) % BufferSize;
+			}
+		}
+		mx.unlock();
+	}
 };
 
 
@@ -89,83 +94,116 @@ struct AveragingFilter {
 // CLASS
 //
 class ControlRouter {
-    public:
-        ControlRouter();
-        ~ControlRouter();
+	public:
+		ControlRouter();
+		~ControlRouter();
 
-        bool Initialize(const ros::NodeHandle& n);
+		bool Initialize(const ros::NodeHandle& n);
 
-        // Switch controller
-        bool ctrl_select_callback(
-                control_router::SelectController::Request  &req,
-                control_router::SelectController::Response &res);
+		// Switch controller
+		bool ctrl_select_callback(
+				control_router::SelectController::Request  &req,
+				control_router::SelectController::Response &res);
 
-        bool enable_network_controllers(
-                control_router::EnableNWController::Request& req,
-                control_router::EnableNWController::Response& res);
-       
-    private:
-        std::mutex mx;
+		bool enable_network_controllers(
+				control_router::EnableNWController::Request& req,
+				control_router::EnableNWController::Response& res);
 
-        // Networked Control Enable
-        bool enabled_;
-
-        // Current controller
-        int current_controller_;
-
-        // Current control signal
-        testbed_msgs::ControlStamped curr_control_;
-        crazyflie_driver::PWM curr_pwm_control_;
-
-        // Load Parameters
-        bool LoadParameters(const ros::NodeHandle& n);
-        
-        bool RegisterCallbacks(const ros::NodeHandle& n);
+	private:
+		std::mutex mx;
+		ros::NodeHandle node_;
 
 
-        // Helper
-        control_router::NetworkStatusMsg generate_network_status_msg();
+		std::string area_name_;
+		std::string target_name_;
 
-        // Service Server
-        ros::ServiceServer ctrl_select_srv_;
-        ros::ServiceServer ctrl_enable_srv_;
+		// Networked Control Enable
+		bool enabled_;
 
-        // Topic subscription
-        ros::Subscriber control_sub_;
-        ros::Subscriber control2_sub_;
-        ros::Subscriber dd_control_sub_;
-        ros::Subscriber dd_control2_sub_;
+		// Current controller
+		std::string current_controller_;
 
-        // Topic publication
-        ros::Publisher control_pub_;
-        ros::Publisher control_pwm_pub_;
-        ros::Publisher network_status_pub_;
-        
-        // Topics callbacks
-        void update_control_callback(
-                const testbed_msgs::ControlStamped::ConstPtr& msg);
-        void update_control2_callback(
-                const testbed_msgs::ControlStamped::ConstPtr& msg);
-        void update_dd_control_callback(
-                const crazyflie_driver::PWM::ConstPtr& msg);
-        void update_dd_control2_callback(
-                const crazyflie_driver::PWM::ConstPtr& msg);
+		// Current control signal
+		testbed_msgs::ControlStamped curr_control_;
+		crazyflie_driver::PWM curr_pwm_control_;
 
-        std::vector<AveragingFilter> probes;
+		// Load Parameters
+		bool LoadParameters(const ros::NodeHandle& n);
 
-        // Names and topics
-        std::string name_;
-        std::string vehicle_name_;
-        std::string output_control_topic_;
-        std::string output_control_pwm_topic_;
-        std::string input_control_topic_;
-        std::string input_control2_topic_;
-        std::string input_dd_control_topic_;
-        std::string input_dd_control2_topic_;
-        std::string network_status_topic_;
+		bool RegisterCallbacks();
+
+		int UpdateControlPublishers();
+
+		bool AssociateTopicsToCallbacks(const ros::NodeHandle& n);
+
+		int UpdatePublishers();
+
+		void net_discovery(int ms);
+
+		// Network Parser
+		NetworkParser network_parser;
+
+		// Helper
+		control_router::NetworkStatusMsg generate_network_status_msg();
+
+		// Service Server
+		ros::ServiceServer ctrl_select_srv_;
+		ros::ServiceServer ctrl_enable_srv_;
+
+		// Topic subscription
+
+		/**
+		 * MAP containing sensor topic information
+		 */
+		std::unordered_map<std::string, TopicData> outchannels_; 
+
+		/**
+		 * MAP for active subscribers
+		 */
+		std::unordered_map<std::string, ros::Subscriber> active_subscriber; 
+		/*
+		   ros::Subscriber control_sub_;
+		   ros::Subscriber control2_sub_;
+		   ros::Subscriber dd_control_sub_;
+		   ros::Subscriber dd_control2_sub_;
+		   */
+
+		// Topic publication
+		ros::Publisher control_pub_;
+		ros::Publisher control_pwm_pub_;
+		ros::Publisher network_status_pub_;
+
+		// Topics callbacks
+		void update_control_callback(
+				const boost::shared_ptr<testbed_msgs::ControlStamped const>& msg,
+				void* arg);
+		void update_pwm_control_callback(
+				const boost::shared_ptr<crazyflie_driver::PWM const>& msg,
+				void* arg);
+
+		std::vector<AveragingFilter> probes;
+
+		// Names and topics
+		std::string name_;
+		std::string vehicle_name_;
+
+		std::string output_control_topic_;
+		std::string output_control_pwm_topic_;
+
+		std::string network_status_topic_;
+
+		/*
+		   std::string input_control_topic_;
+		   std::string input_control2_topic_;
+		   std::string input_dd_control_topic_;
+		   std::string input_dd_control2_topic_;
+		   */
+
+		std::thread net_disc_thr;
 
 
-        bool initialized_;
+		bool initialized_;
+		bool ready_;
 };
 
 #endif
--- ws/src/control_router/src/control_router.cpp
+++ ws/src/control_router/src/control_router.cpp
@@ -7,11 +7,11 @@
 // =================================================================
 // =================================================================
 ControlRouter::ControlRouter() {
-        current_controller_ = 1;
-        for (int i = 0; i < 3; i++) {
-            probes.push_back(AveragingFilter());
-        }
-        enabled_ = false;
+	for (int i = 0; i < 3; i++) {
+		probes.push_back(AveragingFilter());
+	}
+	enabled_ = false;
+	ready_ = false;
 }
 
 ControlRouter::~ControlRouter() {
@@ -20,212 +20,269 @@ ControlRouter::~ControlRouter() {
 
 bool ControlRouter::LoadParameters(const ros::NodeHandle& n) {
 
-    ros::NodeHandle np("~");
-    np.param<std::string>("param/vehicle_name", vehicle_name_,"cf1");
+	ros::NodeHandle np("~");
+	np.param<std::string>("param/vehicle_name", vehicle_name_, "cf1");
 
-    // XXX I should make a search online like for the state_aggregator
-    // XXX I temporarily hard code things here
-    np.param<std::string>("topics/input_ctrl", input_control_topic_,
-            "/area0/controller/geometric_ctrl/" + vehicle_name_ + "/control");
-    np.param<std::string>("topics/input_ctrl2", input_control2_topic_,
-            "/area0/controller/geometric_ctrl2/" + vehicle_name_ + "/control");
-    
-    np.param<std::string>("topics/input_dd_ctrl", input_dd_control_topic_,
-            "/area0/controller/DD_ctrl1/" + vehicle_name_ + "/cmd_pwm");
-    np.param<std::string>("topics/input_dd_ctrl2", input_dd_control2_topic_,
-            "/area0/controller/DD_ctrl2/" + vehicle_name_ + "/cmd_pwm");
-
-    // Output
-    np.param<std::string>("topics/output_ctrl", output_control_topic_,
-            "/" + vehicle_name_ + "/control");
-    np.param<std::string>("topics/output_ctrl_pwm", output_control_pwm_topic_,
-            "/" + vehicle_name_ + "/cmd_pwm");
-    np.param<std::string>("topics/output_network_status", network_status_topic_,
-            "/" + vehicle_name_ + "/network_ctrl_status");
-    return true;
+	// Output
+	np.param<std::string>("topics/output_ctrl", output_control_topic_,
+			"/" + vehicle_name_ + "/control");
+	np.param<std::string>("topics/output_ctrl_pwm", output_control_pwm_topic_,
+			"/" + vehicle_name_ + "/cmd_pwm");
+	np.param<std::string>("topics/output_network_status", network_status_topic_,
+			"/" + vehicle_name_ + "/network_ctrl_status");
+	return true;
 }
 
 /**
  * Initialization function
  */
 bool ControlRouter::Initialize(const ros::NodeHandle& n) {
-    // Get the name of the node
-    name_ = ros::this_node::getName();
+	// Get the name of the node
+	name_ = ros::this_node::getName();
+	node_ = n;
 
-    // Load Parameters
-    if (!LoadParameters(n)) {
-        ROS_ERROR("%s: Failed to load parameters.", name_.c_str());
-        return false;
-    }
-
-    ros::NodeHandle ng(n);
-    control_sub_ = ng.subscribe(
-        input_control_topic_.c_str(), 1, &ControlRouter::update_control_callback, this);
-    control2_sub_ = ng.subscribe(
-        input_control2_topic_.c_str(), 1, &ControlRouter::update_control2_callback, this);
-
-    dd_control_sub_ = ng.subscribe(
-        input_dd_control_topic_.c_str(), 1, &ControlRouter::update_dd_control_callback, this);
-    dd_control2_sub_ = ng.subscribe(
-        input_dd_control2_topic_.c_str(), 1, &ControlRouter::update_dd_control2_callback, this);
+	// Load Parameters
+	if (!LoadParameters(n)) {
+		ROS_ERROR("%s: Failed to load parameters.", name_.c_str());
+		return false;
+	}
 
 
-    // Advertise topics/Services
-    // I want to advertise service in the node namespace
-    ros::NodeHandle nh("~");
-    ctrl_select_srv_ = nh.advertiseService("/" + vehicle_name_ + "/nw_ctrl_select",
-            &ControlRouter::ctrl_select_callback, this);
+	// Register callback
+	if (!RegisterCallbacks()) {
+		ROS_ERROR("%s: Failed to register callbacks.", name_.c_str());
+		return false;
+	}
 
-    ctrl_enable_srv_ = nh.advertiseService("/" + vehicle_name_ + "/nw_ctrl_enable",
-            &ControlRouter::enable_network_controllers, this);
 
-    // Publishers.
-    control_pub_ = ng.advertise<testbed_msgs::ControlStamped>(
-                output_control_topic_.c_str(), 1, false);
+	// Advertise topics/Services
+	// I want to advertise service in the node namespace
+	ros::NodeHandle nh("~");
+	ctrl_select_srv_ = nh.advertiseService(
+			"/" + vehicle_name_ + "/nw_ctrl_select",
+			&ControlRouter::ctrl_select_callback, this);
 
-    control_pwm_pub_ = ng.advertise<crazyflie_driver::PWM>(
-                output_control_pwm_topic_.c_str(), 1, false);
+	ctrl_enable_srv_ = nh.advertiseService(
+			"/" + vehicle_name_ + "/nw_ctrl_enable",
+			&ControlRouter::enable_network_controllers, this);
 
-    network_status_pub_ = ng.advertise<control_router::NetworkStatusMsg>(
-            network_status_topic_.c_str(), 1, false);
+	// Publishers.
+	control_pub_ = node_.advertise<testbed_msgs::ControlStamped>(
+			output_control_topic_.c_str(), 1, false);
 
-    initialized_ = true; 
+	control_pwm_pub_ = node_.advertise<crazyflie_driver::PWM>(
+			output_control_pwm_topic_.c_str(), 1, false);
 
-    return true;
+	network_status_pub_ = node_.advertise<control_router::NetworkStatusMsg>(
+			network_status_topic_.c_str(), 1, false);
+
+
+	net_disc_thr = std::thread(&ControlRouter::net_discovery, this,
+			500);
+
+	initialized_ = true; 
+
+	return true;
+}
+
+
+// Update the list of control publications referring to the 
+// vehicle in the current area.
+bool ControlRouter::RegisterCallbacks() {
+	int npub = UpdatePublishers();
+
+	if (npub > 0) {
+		AssociateTopicsToCallbacks(node_); 
+		ready_ = true;
+		if (current_controller_.empty()) {
+			current_controller_ = outchannels_.begin()->second.node_name;
+		}
+	}
+
+	return true;
+}
+
+
+int ControlRouter::UpdatePublishers() {
+	std::unordered_map<std::string, ros::master::TopicInfo> sdata;
+	network_parser.query_controllers(sdata, area_name_, target_name_);
+
+	for (auto el : sdata) {
+		std::string sname = el.first; 
+		if (outchannels_.count(sname) == 0) {
+			std::cout << "[CONTROL ROUTER] Connecting to Controller: " <<
+				sname << std::endl;
+			TopicData str;
+			str.topic_name = el.second.name;
+			str.area_name = area_name_;
+			str.node_name = sname;
+			str.datatype = el.second.datatype;
+			str.frequency = 0.0;
+			str.isActive = true;
+			str.enabled = true;
+			outchannels_.insert(
+					std::pair<std::string, TopicData>(
+						sname, str)
+					);
+		}
+	}
+	return sdata.size();
+}
+
+bool ControlRouter::AssociateTopicsToCallbacks(const ros::NodeHandle& n) {
+	ros::NodeHandle nl(n);
+	for (auto el : outchannels_) { // For every registered channel
+		std::string topic_name = el.second.topic_name;
+		std::string topic_datatype = el.second.datatype;
+		if (el.second.enabled) { // If the channel is not disabled
+			if (active_subscriber.count(el.first) == 0) { // Associate the callback
+				if (topic_datatype == "testbed_msgs/ControlStamped") {
+					active_subscriber.insert(
+							std::pair<std::string, ros::Subscriber>(
+								el.first,
+								nl.subscribe<testbed_msgs::ControlStamped>(
+									topic_name.c_str(),
+									5, 
+									boost::bind(&ControlRouter::update_control_callback, this, _1,
+										(void*)&outchannels_[el.first].node_name),
+									ros::VoidConstPtr(),
+									ros::TransportHints().tcpNoDelay()
+									)
+								)
+							);
+				}
+
+				if (topic_datatype == "crazyflie_driver/PWM") {
+					active_subscriber.insert(
+							std::pair<std::string, ros::Subscriber>(
+								el.first,
+								nl.subscribe<crazyflie_driver::PWM>(
+									topic_name.c_str(),
+									5, 
+									boost::bind(&ControlRouter::update_pwm_control_callback, this, _1,
+										(void*)&outchannels_[el.first].node_name),
+									ros::VoidConstPtr(),
+									ros::TransportHints().tcpNoDelay()
+									)
+								)
+							);
+				}
+			}
+		} else { // If the chanell is disabled: unsubscribe
+			if (active_subscriber.count(el.first) > 0) {
+				std::cout << "Unsubscribing from " << el.first << std::endl;
+				active_subscriber[el.first].shutdown();
+				active_subscriber.erase(el.first);
+			}
+		}
+	}
+
+	return true;
 }
 
 
 // Service Callbacks
 bool ControlRouter::ctrl_select_callback(
-        control_router::SelectController::Request  &req,
-        control_router::SelectController::Response &res) {
+		control_router::SelectController::Request  &req,
+		control_router::SelectController::Response &res) {
 
-    current_controller_ = req.sel_controller;
+	current_controller_ = req.sel_controller;
 
-    res.curr_controller = current_controller_;
+	res.curr_controller = current_controller_;
 
-    std::cout << "Selected Network Controller: " <<
-        current_controller_ << std::endl;
+	std::cout << "Selected Network Controller: " <<
+		current_controller_ << std::endl;
 
-    control_router::NetworkStatusMsg msg = generate_network_status_msg();
+	control_router::NetworkStatusMsg msg = generate_network_status_msg();
 
-    network_status_pub_.publish(msg);
+	network_status_pub_.publish(msg);
 
-    return true;
+	return true;
 }
 
 bool ControlRouter::enable_network_controllers(
-        control_router::EnableNWController::Request  &req,
-        control_router::EnableNWController::Response &res) {
+		control_router::EnableNWController::Request  &req,
+		control_router::EnableNWController::Response &res) {
 
-    enabled_ = req.enable_nwctrl;
+	enabled_ = req.enable_nwctrl;
 
-    if (enabled_) {
-        std::cout << "Enabled Network Controller!" << std::endl;
-    } else {
-        std::cout << "Disabled Network Controller!" << std::endl;
-    }
+	if (enabled_) {
+		std::cout << "Enabled Network Controller!" << std::endl;
+	} else {
+		std::cout << "Disabled Network Controller!" << std::endl;
+	}
 
-    return enabled_;
+	res.status = enabled_;
+
+	return enabled_;
 }
 
 
 
-//XXX There will be a more complex selection...now, I just want to have 
-//simple to test the control switching between onboard/offboard
 void ControlRouter::update_control_callback(
-        const testbed_msgs::ControlStamped::ConstPtr& msg) {
+		const boost::shared_ptr<testbed_msgs::ControlStamped const>& msg, void* arg) {
 
-    if (current_controller_ == 1) {
-        curr_control_.control.thrust = msg->control.thrust;
-        curr_control_.control.roll = msg->control.roll;  
-        curr_control_.control.pitch = msg->control.pitch;
-        curr_control_.control.yaw_dot = msg->control.yaw_dot;
+	std::string node_name = *(std::string*) arg;
 
-        if (enabled_) {
-            testbed_msgs::ControlStamped out_msg;
-            out_msg = *msg;
-            control_pub_.publish(out_msg);
-        }
-    }
+	if (current_controller_ == node_name) {
+		curr_control_.control.thrust = msg->control.thrust;
+		curr_control_.control.roll = msg->control.roll;  
+		curr_control_.control.pitch = msg->control.pitch;
+		curr_control_.control.yaw_dot = msg->control.yaw_dot;
+
+		if (enabled_) {
+			testbed_msgs::ControlStamped out_msg;
+			out_msg = *msg;
+			control_pub_.publish(out_msg);
+		}
+	}
 }
 
-void ControlRouter::update_control2_callback(
-        const testbed_msgs::ControlStamped::ConstPtr& msg) {
+void ControlRouter::update_pwm_control_callback(
+		const boost::shared_ptr<crazyflie_driver::PWM const>& msg, void* arg) {
 
-    if (current_controller_ == 2) {
-        curr_control_.control.thrust = msg->control.thrust;
-        curr_control_.control.roll = msg->control.roll;  
-        curr_control_.control.pitch = msg->control.pitch;
-        curr_control_.control.yaw_dot = msg->control.yaw_dot;
+	std::string node_name = *(std::string*) arg;
 
-        if (enabled_) {
-            testbed_msgs::ControlStamped out_msg;
-            out_msg = *msg;
-            control_pub_.publish(out_msg);
-        }
-    }
-}
+	ros::Time now = ros::Time::now();
+	probes.at(0).addMeas(now.toSec());
 
-//XXX There will be a more complex selection...now, I just want to have 
-//simple to test the control switching between onboard/offboard
-void ControlRouter::update_dd_control_callback(
-        const crazyflie_driver::PWM::ConstPtr& msg) {
+	if (current_controller_ == node_name) {
+		curr_pwm_control_.pwm0 = msg->pwm0;
+		curr_pwm_control_.pwm1 = msg->pwm1;
+		curr_pwm_control_.pwm2 = msg->pwm2;
+		curr_pwm_control_.pwm3 = msg->pwm3;
 
-    ros::Time now = ros::Time::now();
-    probes.at(0).addMeas(now.toSec());
-    if (current_controller_ == 3) {
-        curr_pwm_control_.pwm0 = msg->pwm0;
-        curr_pwm_control_.pwm1 = msg->pwm1;
-        curr_pwm_control_.pwm2 = msg->pwm2;
-        curr_pwm_control_.pwm3 = msg->pwm3;
+		if (enabled_) {
+			probes.at(2).addMeas(now.toSec());
+			crazyflie_driver::PWM out_msg;
+			out_msg = *msg;
+			control_pwm_pub_.publish(out_msg);
+		}
+	}
 
-        if (enabled_) {
-            probes.at(2).addMeas(now.toSec());
-            crazyflie_driver::PWM out_msg;
-            out_msg = *msg;
-            control_pwm_pub_.publish(out_msg);
-        }
-    }
+	control_router::NetworkStatusMsg net_msg = generate_network_status_msg();
+	network_status_pub_.publish(net_msg);
 
-    control_router::NetworkStatusMsg net_msg = generate_network_status_msg();
-    network_status_pub_.publish(net_msg);
-
-}
-
-void ControlRouter::update_dd_control2_callback(
-        const crazyflie_driver::PWM::ConstPtr& msg) {
-
-    ros::Time now = ros::Time::now();
-    probes.at(1).addMeas(now.toSec());
-    if (current_controller_ == 4) {
-        curr_pwm_control_.pwm0 = msg->pwm0;
-        curr_pwm_control_.pwm1 = msg->pwm1;
-        curr_pwm_control_.pwm2 = msg->pwm2;
-        curr_pwm_control_.pwm3 = msg->pwm3;
-
-        if (enabled_) {
-            probes.at(2).addMeas(now.toSec());
-            crazyflie_driver::PWM out_msg;
-            out_msg = *msg;
-            control_pwm_pub_.publish(out_msg);
-        }
-    }
-    control_router::NetworkStatusMsg net_msg = generate_network_status_msg();
-    network_status_pub_.publish(net_msg);
 }
 
 control_router::NetworkStatusMsg ControlRouter::generate_network_status_msg() {
-    control_router::NetworkStatusMsg msg;
+	control_router::NetworkStatusMsg msg;
 
-    msg.header.stamp = ros::Time::now();
+	msg.header.stamp = ros::Time::now();
 
-    msg.network_ctrl_active = enabled_;
-    msg.active_controller_id = current_controller_;
+	msg.network_ctrl_active = enabled_;
+	msg.active_controller_id = current_controller_;
 
-    for (int i = 0; i < probes.size(); i++) {
-        msg.msgs_freq.push_back(probes.at(i).getAvgFreq()); 
-    }
+	for (int i = 0; i < probes.size(); i++) {
+		msg.msgs_freq.push_back(probes.at(i).getAvgFreq()); 
+	}
 
-    return msg;
+	return msg;
+}
+
+void ControlRouter::net_discovery(int ms) {
+	while (ros::ok()) {
+		RegisterCallbacks();
+		std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+	}
 }
