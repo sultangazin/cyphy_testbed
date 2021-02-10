cc18a6d0ebf1b5be92f320607b1e23bf4008cc8a
Luigi Pannocchi <l.pannocchi@gmail.com>
2021-02-10 01:02:02 -0800

    Updated script

--- ws/scripts/cis_script.sh
+++ ws/scripts/cis_script.sh
@@ -1,4 +1,4 @@
 rosservice call /cf2/Commander_Node/ctrl_offboard_srv "offboard_active: true"
 
-rosservice call /cf2/Commander_Node/goTo_srv "[2.5, 2.0, 0.7]" "8.0" "false"
+rosservice call /cf2/Commander_Node/goTo_srv "[2.5, 2.0, 0.7]" "15.0" "false"
 
