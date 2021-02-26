601e3d3932ef497c618222ae0ff50566401621df
CyphyLab <cyphylab@seas.ucla.edu>
2021-02-25 20:56:59 -0800

    experiment script

--- ws/scripts/cis_goto.sh
+++ ws/scripts/cis_goto.sh
@@ -4,13 +4,13 @@ sleep 1
 rosservice call /cf2/Commander_Node/ctrl_offboard_srv "offboard_active: true"
 
 echo "Take off"
-rosservice call /cf2/Commander_Node/goTo_srv "[-1.0, -0.8, 0.3]" "3.0" "false"
+rosservice call /cf2/Commander_Node/goTo_srv "[-1.35, -1.35, 0.7]" "3.0" "false"
 sleep 3
 
-rosservice call /cf2/Commander_Node/goTo_srv "[0.8, 0.8, 0.3]" "10.0" "false"
-sleep 10
+rosservice call /cf2/Commander_Node/goTo_srv "[1.0, 0.3, 0.7]" "9.0" "false"
+sleep 9
 
-rosservice call /cf2/Commander_Node/land_srv "3.0" "[0.8, 0.8, 0]" "false"
+rosservice call /cf2/Commander_Node/land_srv "3.0" "[1.0, 0.3, 0]" "false"
 sleep 4
 
 rosservice call /cf2/reboot "groupMask: 0"
